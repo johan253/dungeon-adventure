900b6d49938d97e737ab7ed3a071cf32dd37377f
johan253 <johannjo2000@gmail.com>
2024-04-19 17:03:14 -0700

    Implemented some class headers and methods

--- src/model/DatabaseDriver.py
+++ src/model/DatabaseDriver.py
@@ -1,3 +1,25 @@
 class DatabaseDriver:
-    def __call__(self, *args, **kwargs):
+    """
+    A Singleton class used to communicate with the database
+
+    Attributes:
+        _instance: An instance of the DatabaseDriver class
+
+    Methods:
+        get_stats(): Gets the stats of the given DungeonCharacter class
+    """
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Overridden __new__ method to create an instance of the DatabaseDriver class
+        using the Singleton Design pattern
+        :param args:
+        :param kwargs:
+        """
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def get_stats(self, dungeon_character: type):
         pass
---
+++ src/model/DungeonCharacter.py
@@ -0,0 +1,15 @@
+class DungeonCharacter:
+    def __init__(self):
+        self.myName = ""          # String variable for character's name
+        self.myHealth = 0         # Integer variable for character's health
+        self.myDamageMin = 0      # Integer variable for minimum damage
+        self.myDamageMax = 0      # Integer variable for maximum damage
+        self.myAttackSpeed = 0    # Integer variable for attack speed
+        self.myChanceToHit = 0.0  # Float variable for chance to hit
+
+    def attack(self,other):
+        pass
+
+    def getName(self):
+        return self 
+
