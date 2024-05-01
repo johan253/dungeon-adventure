76136cf3296cf210682848484a0c4f0e8cce422e
Pr1nceAli <alybadr074@gmail.com>
2024-05-01 16:42:08 -0700

    Added skeleton of CreateDungeonEntity.py.

---
+++ src/model/CreateDungeonEntity.py
@@ -0,0 +1,55 @@
+from random import randint
+
+
+class CreateDungeonEntity:
+    def __init__(self, database):
+        self.data = database
+
+
+def new_dungeon_wall(self, database):
+    pass
+
+
+def new_north_door(self, database):
+    pass
+
+
+def new_south_door(self, database):
+    pass
+
+
+def new_west_door(self, database):
+    pass
+
+
+def new_east_door(self, database):
+    pass
+
+
+def new_player(self, database):
+    pass
+
+
+def new_monster(self, database):
+    pass
+
+
+def new_health_potion(self, database):
+    pass
+
+
+def new_visual_potion(self, database):
+    pass
+
+
+def new_pillar(self, database):
+    pass
+
+
+def new_exit(self, database):
+    pass
+
+
+if __name__ == "__main__":
+    data = {}
+    factory = CreateDungeonEntity(data)
