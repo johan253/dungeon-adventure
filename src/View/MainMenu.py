88f3bfec8cbe4a8a8a42e66c3cad049031e6ea89
Pr1nceAli <alybadr074@gmail.com>
2024-05-01 17:35:11 -0700

    Moved assets folder into View & updated MainMenu.

---
+++ src/View/MainMenu.py
@@ -0,0 +1,40 @@
+
+import pygame
+from pygame.locals import *
+'''https://www.deviantart.com/lil-mz/art/The-Dungeon-Background-164131346'''
+
+
+class MainMenu:
+    def __init__(self):
+        pygame.init()
+        self.hero_select_visible = False
+        self.load_game_visible = False
+        self.font = pygame.font.SysFont('Arial', 32)
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption('Main Menu')
+        pygame.mouse.set_visible(True)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    return
+
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(pygame.image.load("Assets/mainmenu.jpg"), (0, 0))
+
+            cursor = pygame.mouse.get_pos()
+            if self.hero_select_visible:
+                self.hero_select_visible = True
+            elif self.load_game_visible:
+                self.load_game_visible = True
+            else:
+                play_button = pygame.Rect(100, 200, 100, 50)
+                load_button = pygame.Rect(100, 200, 100, 50)
+                quit_button = pygame.Rect(100, 200, 100, 50)
+                about_button = pygame.Rect(100, 200, 100, 50)
+
+
+                pygame
+
